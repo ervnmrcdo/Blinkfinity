5f958b0e4ee8edaaea8d8fec90a6a5d3b99eacb0
Ervin Mercado <ervinmercado31@gmail.com>
2024-10-25 19:46:15 +0800

    test

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import { click } from "@testing-library/user-event/dist/click";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            Blinkfinity
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
