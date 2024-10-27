03fe62a25ff4557a044f797d7f4851a0f438bea7
seppi72 <giuseppebeltran72@gmail.com>
2024-10-27 23:17:20 +0800

    fixed h3 headings

--- src/components/Blinks.js
+++ src/components/Blinks.js
@@ -1,20 +1,13 @@
- import React from 'react'
- 
- function Blinks(props) {
-   return (
-     <>
-        <section >
-          <h3>Play</h3>
-          <figure className='blink-item-pic-wrap'>
-          <img src={props.src} alt={props.label} className='blink-item-img' />
-          </figure> 
-          <div className='blink-item-info'>
-          </div>
-          <h3>{props.label}</h3>
-        </section>
-     </>
-   )
- }
- 
- export default Blinks
- 
+import React from 'react'
+
+function Blinks(props) {
+  return (
+    <section>
+      <figure className='blink-item-pic-wrap'>
+        <img src={props.src} alt={props.label} className='blink-item-img' />
+      </figure>
+    </section>
+  )
+}
+
+export default Blinks
--- src/components/Play.js
+++ src/components/Play.js
@@ -5,9 +5,17 @@ import Blinks from './Blinks'
 function Play(props) {
   return (
     <div className="blinks">
-      <div className="blink-container">
-        <div className="blink-wrapper">
-            <Blinks src={props.src}  label={props.label}  />
+      <div className="blink-layout">
+        <div className="side-header">
+          <h3>Play</h3>
+        </div>
+        <div className="blink-container">
+          <div className="blink-wrapper">
+            <Blinks src={props.src} label={props.label} />
+          </div>
+        </div>
+        <div className="side-header">
+          <h3>{props.label}</h3>
         </div>
       </div>
     </div>
